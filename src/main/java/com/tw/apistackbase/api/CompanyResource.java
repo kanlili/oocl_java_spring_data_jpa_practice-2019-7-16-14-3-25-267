678df3453c2c3e6ce18d0edcbe83341c8015dbd0
LILY KAN (ITA-ISDC-ISD-OOCLL/ZHA) <KANLI2@OOCL.COM>
2019-07-17 14:43:25 +0800

    put and post: CompanyResourse

--- src/main/java/com/tw/apistackbase/api/CompanyResource.java
+++ src/main/java/com/tw/apistackbase/api/CompanyResource.java
@@ -1,19 +1,27 @@
 package com.tw.apistackbase.api;
 
 import com.tw.apistackbase.core.Company;
+import com.tw.apistackbase.core.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/companies")
 public class CompanyResource {
-    
+    @Autowired
+     private CompanyRepository companyRepository;
     @GetMapping(produces = {"application/json"})
     public Iterable<Company> list() {
-        return null;
+        return companyRepository.findAll();
     }
     
     @PostMapping(produces = {"application/json"})
     public Company add(@RequestBody Company company) {
-        return null;
+        return companyRepository.save(company);
+    }
+    @PutMapping (produces = {"application/json"})
+    public  Company put(@RequestBody Company company)
+    {
+      return companyRepository.save(company);
     }
 }
